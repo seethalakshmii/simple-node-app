e0c3f06e1f2dcc325888d474da219a91a1845b0a
seetha <seethalakshmikap@gmail.com>
2026-05-18 18:35:06 +0530

    testing ci/cd pipeline

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline Working Successfully - version 11');
+    res.send('CI/CD Pipeline Working Successfully - version 12');
 });
 
 app.listen(PORT, () => {
